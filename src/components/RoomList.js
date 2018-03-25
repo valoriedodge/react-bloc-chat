37ddc81be599219f238007d0922a2eb921c7450d
Valorie <valoriejones@gmail.com>
2018-03-25 17:36:42 -0400

    user can add rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,9 +5,12 @@ class RoomList extends Component {
      super(props);
 	console.log("In roomlist");
      this.state = {
-	rooms: []
+	rooms: [],
+	newRoom: ''
      };
      this.roomsRef = this.props.firebase.ref('rooms');
+     this.handleChange= this.handleChange.bind(this);
+     this.handleSubmit= this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -17,6 +20,18 @@ class RoomList extends Component {
        	this.setState({ rooms: this.state.rooms.concat( room ) })
      });
    }
+   handleChange(event) {
+     this.setState({newRoom: event.target.value});
+   }
+
+   handleSubmit(event) {
+     event.preventDefault();
+     const newRoomName = this.state.newRoom;
+     this.roomsRef.push({
+  	name: newRoomName
+     });
+     this.setState({newRoom: ''});
+   }
 
   render(){
     return (
@@ -25,8 +40,11 @@ class RoomList extends Component {
 	{this.state.rooms.map((room,index) => 
 	  <div key={index}>{room.name}</div>)
 	}
-	<p>Choose your facvorite</p>
-      </section>
+	<form onSubmit={this.handleSubmit}>
+	  <input type="text" value={this.state.newRoom} onChange={this.handleChange}/>
+	  <input type="submit" value="Create New Room"/>
+      	</form>
+	</section>
     );
   }
 }
