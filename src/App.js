12b50a9129659a471ef21040e1aeecda087efb1b
Valorie <valoriejones@gmail.com>
2018-03-25 22:43:35 -0400

    part way filter messages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import * as firebase from "firebase";
+import moment from 'moment';
 import './App.css';
 import config from './config/config';
 import RoomList from './components/RoomList';
+import MessageList from './components/MessageList';
 
 
 firebase.initializeApp(config);
 var database = firebase.database();
-var starCountRef = database.ref('rooms');
-console.log(starCountRef !== null);
-starCountRef.on('value', function(snapshot) {
-  console.log(snapshot.val());
-  console.log("cehck snapshot");
-});
-console.log("hello");
-console.log(database.ref('rooms').child("2"));
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+	rooms: [],
+	currentRoom: "Welcome Room",
+	newRoom: ''
+     };
+     this.roomsRef = database.ref('rooms');
+     this.handleChange= this.handleChange.bind(this);
+     this.handleSubmit= this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+     this.roomsRef.on('child_added', snapshot => {
+	const room = snapshot.val();
+	console.log(room);
+       	room.key = snapshot.key;
+      	this.setState({ rooms: this.state.rooms.concat( room ), currentRoom: room.key})
+     	console.log(this.state.currentRoom);
+     });
+  }
+   selectCurrentRoom(key){
+     console.log(key);
+     this.setState({currentRoom: key});
+   }
+   selectRoom(e){
+     let key = e.target.getAttribute("mumbu");
+     this.selectCurrentRoom(key);
+   }
+
+   handleChange(event) {
+     this.setState({newRoom: event.target.value});
+   }
+
+   handleSubmit(event) {
+     event.preventDefault();
+     const newRoomName = this.state.newRoom;
+     if(newRoomName !== ''){
+       const roomKey = this.roomsRef.push({
+    	  name: newRoomName
+       }).key;
+       this.setState({newRoom: ''
+// 	  currentRoom: roomKey
+       });
+     }
+   }
+   
   render() {
     return (
       <div className="App">
 	<h1>Bloc Chat</h1>
 	<main>
-	  <Route exact path="/" component={() => (<RoomList firebase={database} />)}/>
+	  <h1>{this.state.currentRoom}</h1>
+	  <RoomList handleSubmit={this.handleSubmit} handleChange={this.handleChange} rooms={this.state.rooms} newRoom={this.state.newRoom} selectRoom={(e)=>this.selectRoom(e)} />
+	  <MessageList user="Valorie" roomId={this.state.currentRoom} firebase={database}/>
 	</main>
       </div>
     );
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,47 +1,15 @@
 import React, {Component} from 'react';
 
 class RoomList extends Component {
-  constructor(props) {
-     super(props);
-	console.log("In roomlist");
-     this.state = {
-	rooms: [],
-	newRoom: ''
-     };
-     this.roomsRef = this.props.firebase.ref('rooms');
-     this.handleChange= this.handleChange.bind(this);
-     this.handleSubmit= this.handleSubmit.bind(this);
-  }
-
-  componentDidMount() {
-     this.roomsRef.on('child_added', snapshot => {
-	const room = snapshot.val();
-       	room.key = snapshot.key;
-       	this.setState({ rooms: this.state.rooms.concat( room ) })
-     });
-   }
-   handleChange(event) {
-     this.setState({newRoom: event.target.value});
-   }
-
-   handleSubmit(event) {
-     event.preventDefault();
-     const newRoomName = this.state.newRoom;
-     this.roomsRef.push({
-  	name: newRoomName
-     });
-     this.setState({newRoom: ''});
-   }
-
   render(){
     return (
       <section>
 	<h2>Rooms</h2>
-	{this.state.rooms.map((room,index) => 
-	  <div key={index}>{room.name}</div>)
+	{this.props.rooms.map((room,index) => 
+	  <div mumbu={room.key} onClick={this.props.selectRoom} key={room.key}>{room.name}</div>)
 	}
-	<form onSubmit={this.handleSubmit}>
-	  <input type="text" value={this.state.newRoom} onChange={this.handleChange}/>
+	<form onSubmit={this.props.handleSubmit}>
+	  <input type="text" value={this.props.newRoom} onChange={this.props.handleChange}/>
 	  <input type="submit" value="Create New Room"/>
       	</form>
 	</section>
